b41df7ba236bb11ca70d3d451ad21ed95cc23718
Michael Steinert <steinert-michael@gmx.de>
2020-03-17 22:36:05 +0100

    DataService implemented

---
+++ src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,37 @@
+package com.example.coronavirustracker.services;
+
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+
+    @PostConstruct //Annotation bewirkt dass nach Instaziierung die Methode ausgeführt wird.
+    @Scheduled(cron = "* * 1 * * *") //Annotation bewirkt dass Methode jede Stunde (s, m, h, d, m, y) ausgeführt wird.
+    public void fetchVirusData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
+    }
+}
